7ed84bded821a525a17b778fd3fca2d57cee9898
Marco15453 <marcolpder@gmail.com>
2021-09-07 08:43:55 +0200

    Update Unlock.cs

--- ElevatorLockdown/Commands/Unlock.cs
+++ ElevatorLockdown/Commands/Unlock.cs
@@ -28,26 +28,18 @@ namespace ElevatorLockdown.Commands {
                 return false;
             }
 
-            if(arguments.At(0) == "gatea") {
-                if(!ElevatorLockdown.Instance.disabledElevators.Contains(ElevatorType.GateA)) {
-                    response = "Gate A Lift is already enabled by Automatic Failure System";
-                    return false;
-                }
+            if(arguments.At(0) == "gatea" && ElevatorLockdown.Instance.disabledElevators.Contains(ElevatorType.GateA)) {
                 Cassie.Message(ElevatorLockdown.Instance.Config.CassieMessageReactivated.Replace("{GATE}", "Gate A"));
                 ElevatorLockdown.Instance.disabledElevators.Remove(ElevatorType.GateA);
                 response = "Gate A Lift has been reactivated by Administrator!";
                 return true;
-            } else if(arguments.At(0)== "gateb") {
-                if (!ElevatorLockdown.Instance.disabledElevators.Contains(ElevatorType.GateB)) {
-                    response = "Gate B Lift is already enabled by Automatic Failure System";
-                    return false;
-                }
+            } else if(arguments.At(0)== "gateb" && ElevatorLockdown.Instance.disabledElevators.Contains(ElevatorType.GateB)) {
                 Cassie.Message(ElevatorLockdown.Instance.Config.CassieMessageReactivated.Replace("{GATE}", "Gate B"));
                 ElevatorLockdown.Instance.disabledElevators.Remove(ElevatorType.GateB);
                 response = "Gate B Lift has been reactivated by Administrator!";
                 return true;
             } else {
-                response = "Argument must be 'GateA' or 'GateB'!";
+                response = $"{arguments.At(1)} is already deactivated by Automatic Failure System";
                 return false;
             }
         }
