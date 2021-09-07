5dc4f905d4350132284f498f19061412f8f19e92
Marco15453 <marcolpder@gmail.com>
2021-09-07 08:16:30 +0200

    Update PlayerHandler.cs

--- ElevatorLockdown/Events/PlayerHandler.cs
+++ ElevatorLockdown/Events/PlayerHandler.cs
@@ -6,10 +6,9 @@ using System.Collections.Generic;
 namespace ElevatorLockdown.Events {
     internal sealed class PlayerHandler {
         public void onInteractingElevator(InteractingElevatorEventArgs ev) {
-            if(ElevatorLockdown.Instance.disabledElevators.Contains(ev.Lift.Type())) {
-                if (ElevatorLockdown.Instance.Config.HintTime > 0) ev.Player.ShowHint(ElevatorLockdown.Instance.Config.HintMessage, ElevatorLockdown.Instance.Config.HintTime);
-                ev.IsAllowed = false;
-            }
+            if(!ElevatorLockdown.Instance.disabledElevators.Contains(ev.Lift.Type()))
+            if (ElevatorLockdown.Instance.Config.HintTime > 0) ev.Player.ShowHint(ElevatorLockdown.Instance.Config.HintMessage, ElevatorLockdown.Instance.Config.HintTime);
+            ev.IsAllowed = false;
         }
     }
 }
