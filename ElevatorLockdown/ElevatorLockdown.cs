e011c99a259228367aa778885aca5d4217cdf64b
Marco15453 <marcolpder@gmail.com>
2022-02-18 05:39:32 +0100

    EXILED 5.0.0
    # Changelog
    - Updated to EXILED 5.0.0-alpha.21
    
    ***IF YOU ARE USING EXILED 4.2.3 ON YOUR SERVER DO NOT USE THIS VERSION***

--- ElevatorLockdown/ElevatorLockdown.cs
+++ ElevatorLockdown/ElevatorLockdown.cs
@@ -13,8 +13,8 @@ namespace ElevatorLockdown
 
         public override string Author => "Marco15453";
         public override string Name => "ElevatorLockdown";
-        public override Version Version => new Version(1, 9, 0);
-        public override Version RequiredExiledVersion => new Version(4, 2, 2);
+        public override Version Version => new Version(1, 10, 0);
+        public override Version RequiredExiledVersion => new Version(5, 0, 0);
 
         private ServerHandler serverHandler;
         private PlayerHandler playerHandler;
